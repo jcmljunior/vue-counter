73f559e27d66be1f7bf9efd327a731c4ad02677d
Julio Cesar <jcmljunior@proton.me>
2025-01-02 21:04:06 -0300

    Adicionado Lorem ipsum

--- src/views/teste_view.vue
+++ src/views/teste_view.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="counter-view">
-        <p>Teste</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo accusantium, facere sed, iure vitae
+            necessitatibus alias architecto animi natus dolorem reprehenderit eveniet aut. Quidem debitis molestiae ex
+            similique commodi quae.</p>
     </div>
 </template>
